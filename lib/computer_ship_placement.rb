bf80d59386cbcc4753ee78a0e972e3fdb671d644
JoelLindow <joellindow@gmail.com>
2017-05-21 22:34:28 -0600

    building reference catalogues for boat placement

--- lib/computer_ship_placement.rb
+++ lib/computer_ship_placement.rb
@@ -3,24 +3,33 @@ class ComputerShipPlacement
   attr_reader :computer_board
 
   def initialize
-    @computer_board = populated_computer_board
+    @computer_board = [
+                        [" . ", " . ", " . ", " . "],
+                        [" . ", " . ", " . ", " . "],
+                        [" . ", " . ", " . ", " . "],
+                        [" . ", " . ", " . ", " . "]
+                      ]
   end
   # def self.ship_start_point(computer_board)
   #   computer_board.data[rand(0..3)][rand(0..3)] = " B "
   # end
   def place_boats
-    if boat_direction == 0
-      horizontal_boat
-    else
-      vertical_boat
+    boat_lengths = [2, 3]
+    until boat_lengths.empty?
+      if boat_direction == 0
+        horizontal_boat(boat_lengths[0])
+      else
+        vertical_boat(boat_lengths[0])
+      end
+      boat_lengths.shift!
     end
   end
 
-  def horizontal_boat
+  def horizontal_boat(boat_length)
     puts "horizontal_boat"
   end
 
-  def vertical_boat
+  def vertical_boat(boat_length)
     puts "vertical_boat"
   end
 
@@ -28,8 +37,17 @@ class ComputerShipPlacement
     rand(0..1)
   end
 
-  def populated_computer_board
-    "THIS IS THE POPULATED COMPUTER BOARD!!!!!!!!!!!!!!!"
+  def two_boat_start_index
+    rand(0..2)
+  end
+
+  def three_boat_start_index
+    rand(0..1)
+  end
+
+  def populated_computer_board #<--- game board calls this for computer game board
+    place_boats
+    computer_board
   end
 
   def empty_game_board
@@ -41,7 +59,26 @@ class ComputerShipPlacement
     ]
   end
 
+  def board_reference_horizontal
+    [
+      [[0, 0], [0, 1], [0, 2], [0, 3]],
+      [[1, 0], [1, 1], [1, 2], [1, 3]],
+      [[2, 0], [2, 1], [2, 2], [2, 3]],
+      [[3, 0], [3, 1], [3, 2], [3, 3]]
+    ]
+
+  end
+
+  def board_reference_vertical
+    [
+      [[3, 0], [2, 0], [1, 0], [0, 0]],
+      [[3, 1], [2, 1], [1, 1], [0, 1]],
+      [[3, 2], [2, 2], [1, 2], [0, 2]],
+      [[3, 3], [2, 3], [1, 3], [0, 3]]
+    ]
+  end
+
 end
 
-# binding.pry
-# ""
+binding.pry
+""

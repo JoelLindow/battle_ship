5444359117aa562272a1215f6af9c6d1a083f93f
JoelLindow <joellindow@gmail.com>
2017-05-22 18:05:04 -0600

    refactor of 3 long boat broke code. reverting to longer metnod for accuracy

--- lib/computer_ship_placement.rb
+++ lib/computer_ship_placement.rb
@@ -51,9 +51,9 @@ class ComputerShipPlacement
       #0 is horizontal
       boat_2_start_row = rand(0..3)
       boat_2_start_index = rand(0..1)
-      if (@computer_board[boat_2_start_row][boat_2_start_index] &&
-          @computer_board[boat_2_start_row][boat_2_start_index + 1] &&
-          @computer_board[boat_2_start_row][boat_2_start_index + 2]) == " . "
+      if @computer_board[boat_2_start_row][boat_2_start_index] == " . " &&
+          @computer_board[boat_2_start_row][boat_2_start_index + 1] == " . " &&
+          @computer_board[boat_2_start_row][boat_2_start_index + 2] == " . "
         @computer_board[boat_2_start_row][boat_2_start_index] = " B "
         @computer_board[boat_2_start_row][boat_2_start_index + 1] = " B "
         @computer_board[boat_2_start_row][boat_2_start_index + 2] = " B "
@@ -64,7 +64,9 @@ class ComputerShipPlacement
       #1 is vertical
       boat_2_start_row = rand(0..1)
       boat_2_start_index = rand(0..3)
-      if @computer_board[boat_2_start_row][boat_2_start_index] == " . " && @computer_board[boat_2_start_row + 1][boat_2_start_index] == " . " && @computer_board[boat_2_start_row + 2][boat_2_start_index] == " . "
+      if @computer_board[boat_2_start_row][boat_2_start_index] == " . " &&
+          @computer_board[boat_2_start_row + 1][boat_2_start_index] == " . " &&
+          @computer_board[boat_2_start_row + 2][boat_2_start_index] == " . "
         @computer_board[boat_2_start_row][boat_2_start_index] = " B "
         @computer_board[boat_2_start_row + 1][boat_2_start_index] = " B "
         @computer_board[boat_2_start_row + 2][boat_2_start_index] = " B "

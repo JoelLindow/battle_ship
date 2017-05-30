fc8a86eba7b47002fc57946fb0f496f9ba180438
JoelLindow <joellindow@gmail.com>
2017-05-30 09:11:27 -0600

    game plays through with possibility for invalid input glitches

--- lib/battleship_menu.rb
+++ lib/battleship_menu.rb
@@ -25,9 +25,24 @@ class BattleshipMenu
       system "clear"
       # game.game_start_time = Time.now or something like that.
       game.activate_game_play
+      play_again?
     else
       system "clear"
       start_menu
+    end
+  end
+
+  def play_again?
+    puts "Want to Play Again?  (y)es   (n)o"
+    input = gets.chomp.downcase
+    if input == "y"
+      start_menu
+    elsif input == "n"
+      system "clear"
+      puts Messages.quit_game_message
+    else
+      puts "Invalid Option! Try Again"
+      play_again?
     end
   end
 
